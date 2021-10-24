b7027f32d59338cb502e6a7b900e6c1b584d9fd6
a-korkin <a-korkin88@yandex.ru>
2021-10-24 10:17:21 +0300

    icons added

--- src/ui/Select/Select.tsx
+++ src/ui/Select/Select.tsx
@@ -1,19 +1,45 @@
 import React from "react";
+import { FaAngleDown } from "react-icons/fa";
 import "./Select.scss";
 
 const Select: React.FC = () => {
     return (
-        <div className="select-wrapper">
-            <div className="select">
-                <div className="select__trigger">
-                    <span>Tesla</span>
-                    <div className="arrow"></div>
+        <div className="select-box">
+            
+            <div className="options-container active">
+                <div className="option">
+                    <input type="radio" name="category" id="automobiles" className="radio" />
+                    <label htmlFor="automobiles">Automobiles</label>
                 </div>
-                <div className="custom-options">
-                    <span className="custom-option selected" data-value="tesla">Tesla</span>
-                    <span className="custom-option" data-value="volvo">Volvo</span>
-                    <span className="custom-option" data-value="mercedes">Mercedes</span>
+                <div className="option">
+                    <input type="radio" name="category" id="animations" className="radio" />
+                    <label htmlFor="animations">Film & animations</label>
                 </div>
+                <div className="option">
+                    <input type="radio" name="category" id="technology" className="radio" />
+                    <label htmlFor="technology">Science & technology</label>
+                </div>
+                <div className="option">
+                    <input type="radio" name="category" id="art" className="radio" />
+                    <label htmlFor="art">Art</label>
+                </div>
+                <div className="option">
+                    <input type="radio" name="category" id="sports" className="radio" />
+                    <label htmlFor="sports">Sports</label>
+                </div>
+                <div className="option">
+                    <input type="radio" name="category" id="news" className="radio" />
+                    <label htmlFor="news">News</label>
+                </div>
+                <div className="option">
+                    <input type="radio" name="category" id="music" className="radio" />
+                    <label htmlFor="music">Music</label>
+                </div>
+            </div>
+
+            <div className="selected">
+                Select video category
+                <FaAngleDown />
             </div>
         </div>
     );
