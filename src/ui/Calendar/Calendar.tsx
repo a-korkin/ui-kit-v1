08eb35c3ece582d003d9295cb80d3cb097ee6f6e
a-korkin <a-korkin88@yandex.ru>
2021-10-26 16:06:59 +0300

    active month

--- src/ui/Calendar/Calendar.tsx
+++ src/ui/Calendar/Calendar.tsx
@@ -14,7 +14,7 @@ const Calendar: React.FC<ICalendarProps> = ({currentDate}) => {
         [2, "February"],
         [3, "March"],
         [4, "April"],
-        [6, "May"],
+        [5, "May"],
         [6, "June"],
         [7, "July"],
         [8, "August"],
@@ -57,22 +57,43 @@ const Calendar: React.FC<ICalendarProps> = ({currentDate}) => {
     const [date, setDate] = useState<IDate>(_date);
     const [days, setDays] = useState<number[]>(createDays(daysInMonth.get(_date.month.id) ?? 30));
     const [activeDay, setActiveDay] = useState<number>(_date.day);
+    const [activeMonth, setActiveMonth] = useState<boolean>(false);
 
     const changeYearHandler = (e: React.MouseEvent<HTMLSpanElement>, type: string) => {
         switch (type) {
             case "add":
                 setDate(prevState => ({...prevState, year: prevState.year + 1}));
                 break;
-            default:
+            case "remove":
                 setDate(prevState => ({...prevState, year: prevState.year - 1}));
                 break;
         }
     }
 
+    const changeMonthHandler = (e: React.MouseEvent<HTMLButtonElement>, key: number) => {
+        setDate(prevState => ({
+            ...prevState, month: {id: key, name: months.get(key)}
+        }));
+        setActiveMonth(false);
+        setDays(createDays(daysInMonth.get(key) ?? 30));
+    }
+
     return (
         <div className="calendar">
+            <div className={activeMonth ? "calendar__months active" : "calendar__months"}>
+                {
+                    Array.from(months).map(([key, value]) => 
+                        <button 
+                            className="calendar__months-item" 
+                            key={key}
+                            onClick={e => changeMonthHandler(e, key)}
+                        >
+                            {value}
+                        </button>)
+                }
+            </div>
             <div className="calendar__year-month">
-                <div className="month">{date.month.name}</div>
+                <button className="month" onClick={e => setActiveMonth(!activeMonth)}>{date.month.name}</button>
                 <div className="year">
                     <button onClick={e => changeYearHandler(e, "remove")}><FaAngleLeft /></button>
                     <span>{date.year}</span>
@@ -96,40 +117,10 @@ const Calendar: React.FC<ICalendarProps> = ({currentDate}) => {
                             className={activeDay === day ? "calendar__days-item active" : "calendar__days-item"}
                             onClick={e => setActiveDay(day)}
                         >
-                            {day}<span></span><span></span><span></span><span></span>
+                            {day}
+                            <span></span><span></span><span></span><span></span>
                         </div>)
                 }
-
-                {/* <div className="calendar__days-item" onClick={activeDayHandler}>1<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">2<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">3<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">4<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">5<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">6<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">7<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">8<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">9<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">10<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">11<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">12<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">13<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">14<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">15<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">16<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">17<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">18<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">19<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item active">20<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">21<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">22<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">23<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">24<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">25<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">26<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">27<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">28<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">29<span></span><span></span><span></span><span></span></div>
-                <div className="calendar__days-item">30<span></span><span></span><span></span><span></span></div> */}
             </div>
         </div>
     );
