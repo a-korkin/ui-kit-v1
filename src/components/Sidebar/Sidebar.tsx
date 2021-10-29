6e16a87b2d2549aa44151f0e7f7191a9d36272e0
a-korkin <a-korkin88@yandex.ru>
2021-10-29 16:26:55 +0300

    styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -15,55 +15,55 @@ const Sidebar: React.FC<ISidebarProps> = ({collapse}) => {
                 <div className="menu">
                     <div className="menu__item">
                         <input className="menu__item-input" type="checkbox" name="A" id="A" />
-                        <label className="menu__item-label" htmlFor="A"><FaAngleRight />Services</label>
+                        <label className="menu__item-label menu__item--clickable" htmlFor="A"><FaAngleRight />Services</label>
 
                         <ul className="item__list">
-                            <li><a className="item-link" href="#">Branding</a></li>
-                            <li><a className="item-link" href="#">Web/App development</a></li>
-                            <li><a className="item-link" href="#">Internet Marketing and SEO</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Branding</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Web/App development</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Internet Marketing and SEO</a></li>
                         </ul>
                     </div>
 
                     <div className="menu__item">
                         <input className="menu__item-input" type="checkbox" name="B" id="B" />
-                        <label className="menu__item-label" htmlFor="B"><FaAngleRight />Jobs</label>
+                        <label className="menu__item-label menu__item--clickable" htmlFor="B"><FaAngleRight />Jobs</label>
                         <ul className="item__list">
                             <li>
                                 <div className="menu__item">
                                     <input className="menu__item-input" type="checkbox" name="BA" id="BA" />
-                                    <label className="menu__item-label" htmlFor="BA"><FaAngleRight />UI/UX</label>
+                                    <label className="menu__item-label menu__item--clickable" htmlFor="BA"><FaAngleRight />UI/UX</label>
 
                                     <ul className="item__list">
-                                        <li><a className="item-link" href="#">UI/UX Designer</a></li>
-                                        <li><a className="item-link" href="#">UI Designer</a></li>
-                                        <li><a className="item-link" href="#">UX Designer</a></li>
+                                        <li className="menu__item--clickable"><a className="item-link" href="#">UI/UX Designer</a></li>
+                                        <li className="menu__item--clickable"><a className="item-link" href="#">UI Designer</a></li>
+                                        <li className="menu__item--clickable"><a className="item-link" href="#">UX Designer</a></li>
                                     </ul>
                                 </div>
                             </li>
                             <li>
                                 <div className="menu__item">
                                     <input className="menu__item-input" type="checkbox" name="BB" id="BB" />
-                                    <label className="menu__item-label" htmlFor="BB"><FaAngleRight />Development</label>
+                                    <label className="menu__item-label menu__item--clickable" htmlFor="BB"><FaAngleRight />Development</label>
 
                                     <ul className="item__list">
-                                        <li><a className="item-link" href="#">FrontEnd</a></li>
-                                        <li><a className="item-link" href="#">BackEnd</a></li>
+                                        <li className="menu__item--clickable"><a className="item-link" href="#">FrontEnd</a></li>
+                                        <li className="menu__item--clickable"><a className="item-link" href="#">BackEnd</a></li>
                                     </ul>
                                 </div>
                             </li>
-                            <li><a className="item-link" href="#">Graphic</a></li>
-                            <li><a className="item-link" href="#">Logo Designer</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Graphic</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Logo Designer</a></li>
                         </ul>
                     </div>
 
                     <div className="menu__item">
                         <input className="menu__item-input" type="checkbox" name="C" id="C" />
-                        <label className="menu__item-label" htmlFor="C"><FaAngleRight />About</label>
+                        <label className="menu__item-label menu__item--clickable" htmlFor="C"><FaAngleRight />About</label>
 
                         <ul className="item__list">
-                            <li><a className="item-link" href="#">Our team</a></li>
-                            <li><a className="item-link" href="#">Clents</a></li>
-                            <li><a className="item-link" href="#">Our work</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Our team</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Clents</a></li>
+                            <li className="menu__item--clickable"><a className="item-link" href="#">Our work</a></li>
                         </ul>
                     </div>
 
