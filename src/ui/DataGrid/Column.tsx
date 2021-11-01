7ca107a694648ab373467476f612db0a73fd2368
a-korkin <a-korkin88@yandex.ru>
2021-11-01 07:28:34 +0300

    custom resize

--- src/ui/DataGrid/Column.tsx
+++ src/ui/DataGrid/Column.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { IColumn } from "../../models";
 import { useResizeDetector } from "react-resize-detector";
+// import { ResizableBox } from 'react-resizable';
 
 interface IColumnProps {
     column: IColumn;
@@ -36,22 +37,47 @@ const Column: React.FC<IColumnProps> = ({column, setCurrent, dropColumn, resizeC
         // console.log(`${ref.current.id} - ${ref.current.clientWidth}`);
         resizeColumn(column);
         // console.log(column);
+        // console.log(`${ref.current.clientWidth}`);
     }, []);
 
     const {width, height, ref} = useResizeDetector({ onResize });
 
+
+    const spanRef = useRef<HTMLSpanElement>(null);
+
+    const mouseMoveHandler = (e: React.MouseEvent<HTMLSpanElement>) => {
+        console.log(spanRef, "move");
+    }
+
+    const mouseDownHandler = (e: React.MouseEvent<HTMLSpanElement>) => {
+        // console.log(e.currentTarget.style.width);
+        console.log(spanRef.current?.clientWidth);
+    }
+
     return (
+        // <ResizableBox className="grid-column" width={100} height={100} onResize={e => console.log("dfsf")}>
+        //     {column.name} {width} {height}
+        // </ResizableBox>
+
         <div 
-            ref={ref}
+            // ref={ref}
             className="grid-column"
-            draggable={true}
+            // draggable={true}
             onDragStart={e => dragStartHandler(e, column)}
             onDragLeave={dragLeaveHandler}
             onDragEnd={dragEndHandler}
             onDragOver={dragOverHandler}
             onDrop={e => dropHandler(e, column)}
         >
-            {column.name} {width} {height}
+            <span 
+                ref={spanRef}
+                className="resize-line" 
+                onMouseMove={e => mouseMoveHandler(e)}
+                onMouseDown={e => mouseDownHandler(e)}
+                onMouseUp={e => console.log("up")}
+            ></span>
+            {column.name}
+            {/* {column.name} {width} {height} */}
         </div>
     );
 }
